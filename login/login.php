c393bc95abb64b7b0e584cfb6fe6b45ee9952b0f
srnrss <serena.russo8@studio.unibo.it>
2021-03-17 11:36:01 +0100

    Inital commit login

---
+++ login/login.php
@@ -0,0 +1,8 @@
+ 
+<?php
+    include('/xampp/htdocs/ebiblio/biblioteche/partials/menu.php');
+?>
+
+<?php
+include('/xampp/htdocs/ebiblio/biblioteche/partials/footer.php');
+?>
---
+++ login/partials/menu.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Ebiblio</title>
+</head>
+
+<body>
+    
+    <nav class="navbar navbar-expand-md navbar-dark bg-secondary">
+        <a href="#" class="navbar-brand">
+            <img src="../images/book-half.svg" height="28">
+            UNIBOXEBIBLIO
+        </a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav">
+                <a href="/ebiblio" class="nav-item nav-link">Home</a>
+                <a href="/ebiblio/biblioteche/biblioteche.php" class="nav-item nav-link">Biblioteche</a>
+                <a href="#" class="nav-item nav-link">Statistiche</a>
+
+            </div>
+            <div class="navbar-nav ml-auto">
+                <a href="#" class="nav-item nav-link">Login</a>
+                <a href="#" class="nav-item nav-link">Registrati</a>
+            </div>
+        </div>
+    </nav>
